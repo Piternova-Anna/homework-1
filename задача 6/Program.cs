eae5c03954fba13f7dbcf6df431de7da83a04b75
Anna Piternova <anna-maria999@mail.ru>
2022-08-25 17:56:11 +0300

    'задача8'

---
+++ задача 6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите число");
+double a = double.Parse(Console.ReadLine());
+
+If(a%2=0);
+{
+    Console.WriteLine("Четное число")
+};
+
+else
+  {
+    Console.WriteLine("нечетное число")
+  };
---
+++ задача 8/Program.cs
@@ -0,0 +1,22 @@
+﻿// принимает число N  и выдает все четные от 1 до n;
+Console.WriteLine("введите число n:");
+int n - int.Parse(Console.ReadLine());
+int count=2;
+
+if(n<1),
+{
+Console.WriteLine("ошибка");
+}
+else
+{
+    Console.WriteLine("четные числа от 1 до заданного числа");
+}
+
+while (count<n)
+{
+    if(count%2=0)
+    {
+        Console.WriteLine(count);
+    }
+    count=count+1;
+}
